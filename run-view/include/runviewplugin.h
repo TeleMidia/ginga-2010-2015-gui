52841305e1fef12bff5515400c37dc6e4921124e
Roberto Azevedo <robertogerson@telemidia.puc-rio.br>
2017-11-08 23:14:27 +0100

    runview: Use template-based plugin factory class

--- run-view/include/runviewplugin.h
+++ run-view/include/runviewplugin.h
@@ -2,16 +2,17 @@
 #define RUNVIEWPLUGIN_H
 
 #include <extensions/IPlugin.h>
+#include <extensions/IPluginFactory.h>
 #include <modules/ProjectControl.h>
 
 #include "../include/gingaproxy.h"
 #include "../include/gingaview.h"
 #include "include/configdialog.h"
 
+#include <QAction>
+#include <QMenu>
 #include <QPushButton>
 #include <QToolButton>
-#include <QMenu>
-#include <QAction>
 
 using namespace cpr::core;
 
@@ -24,53 +25,57 @@ class RunViewPlugin : public IPlugin
   Q_OBJECT
 
 public:
-  explicit RunViewPlugin();
-  inline ~RunViewPlugin(){ delete _runWidget; }
-  inline QWidget* widget() override { return _runWidget;}
+  explicit RunViewPlugin ();
+  inline ~RunViewPlugin () { delete _runWidget; }
+  inline QWidget *
+  widget () override
+  {
+    return _runWidget;
+  }
 
 public slots:
   void init () override;
   void loadPreferences ();
   void playApplication ();
-  void runPassiveDevice();
-  void runActiveDevice();
-  void execConfigDialog();
-  void updateGUI();
+  void runPassiveDevice ();
+  void runActiveDevice ();
+  void execConfigDialog ();
+  void updateGUI ();
   void handleError (QProcess::ProcessError error);
 
-  inline bool eventFilter(QObject *obj, QEvent *event) override
+  inline bool
+  eventFilter (QObject *obj, QEvent *event) override
   {
-    if (event->type() == QEvent::Resize)
+    if (event->type () == QEvent::Resize)
     {
-      QString aspectRatio = _settings->value(Util::V_ASPECT_RATIO,
-                                             "").toString();
+      QString aspectRatio
+          = _settings->value (Util::V_ASPECT_RATIO, "").toString ();
       if (aspectRatio == Util::WIDE)
       {
-        _gingaView->setFixedHeight(_gingaView->width() * 9 / 16);
+        _gingaView->setFixedHeight (_gingaView->width () * 9 / 16);
         return true;
       }
       else if (aspectRatio == Util::STANDARD)
       {
-        _gingaView->setFixedHeight(_gingaView->width() * 3 / 4);
+        _gingaView->setFixedHeight (_gingaView->width () * 3 / 4);
         return true;
       }
       else
       {
-        _gingaView->setMinimumSize(0, 0);
-        _gingaView->setMaximumSize(QWIDGETSIZE_MAX, QWIDGETSIZE_MAX);
+        _gingaView->setMinimumSize (0, 0);
+        _gingaView->setMaximumSize (QWIDGETSIZE_MAX, QWIDGETSIZE_MAX);
       }
     }
-    return IPlugin::eventFilter(obj, event);
+    return IPlugin::eventFilter (obj, event);
   }
 
-
 private:
-  ProjectControl * _projectControl;
+  ProjectControl *_projectControl;
   Project *_currentProject;
 
   QWidget *_runWidget;
-  GingaView * _gingaView;
-  GingaProxy * _gingaProxy;
+  GingaView *_gingaView;
+  GingaProxy *_gingaProxy;
 
   QToolButton *_playButton;
   QMenu *menu_Multidevice;
@@ -79,6 +84,25 @@ private:
   QSettings *_settings;
 };
 
-inline Q_LOGGING_CATEGORY(CPR_PLUGIN_RUN, "cpr.plugin.run")
+/*!
+ * \brief Handles the creation and deletion of RunViewPlugin objects.
+ */
+class RunViewFactory : public QObject, public IPluginFactoryTpl<RunViewPlugin>
+{
+  Q_OBJECT
+  CPR_PLUGIN_METADATA ("run-view.json")
+
+public:
+  RunViewFactory () {}
+  ~RunViewFactory () { GingaProxy::deleteInstance (); }
+
+  QString
+  id () const
+  {
+    return "br.puc-rio.telemidia.RunView";
+  }
+};
+
+inline Q_LOGGING_CATEGORY (CPR_PLUGIN_RUN, "cpr.plugin.run")
 
 #endif // RUNVIEWPLUGIN_H
